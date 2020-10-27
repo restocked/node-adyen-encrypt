df3536805a9c153d76fee19ad53dce1382e7c8ec
wervux <riccardobassanelli@gmail.com>
2020-10-27 21:08:28 +0100

    changed index function into constructor

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function adyenEncrypt(version) {
-    version = ~~version;
-
-    if (version && (version >= 22 && version <= 25)) {
-        return require("./lib/0_1_" + version);
+class adyenEncrypt {
+    constructor(version) {
+        version = ~~version;
+        if (version && ((version >= 22 && version <= 25) || version === 18)) {
+            return require("./lib/0_1_" + version);
+        }
+        return require("./lib/0_1_24");
     }
-
-    return require("./lib/0_1_24");
 }
-
 module.exports = adyenEncrypt;
